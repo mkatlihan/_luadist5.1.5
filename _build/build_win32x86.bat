a5701f51630268a3d75db6bbe53e4e30c968bfd0
Mustafa Atlihan <mkatlihan@gmail.com>
2024-12-18 15:55:31 +0300

    peku

--- _build/build_win32x86.bat
+++ _build/build_win32x86.bat
@@ -1,9 +1,11 @@
 rem call "D:\usr\Intel\Compiler\11.1\067\bin\iclvars.bat" ia32
+if not exist win32x86 mkdir win32x86
 rm -rf win32x86/*  
 mv x86.log x86.log.bak
 cd win32x86 
 echo %date% Begin build process | tee ../x86.log
-e:\Mingw\opt\bin\cmake.exe -DXOPT_OUTPUT_DIR=ON -G "NMake Makefiles" %* ..\..  | tee -a ../x86.log
+cmake.exe -DXOPT_OUTPUT_DIR=ON -G "NMake Makefiles" %* ..\..  | tee -a ../x86.log
 nmake | tee -a ../x86.log
 echo %date% End build process | tee -a ../x86.log
+name install | tee -a ../x86.log
 cd .. 
--- _build/build_win64x86.bat
+++ _build/build_win64x86.bat
@@ -1,9 +1,11 @@
 rem call "D:\usr\Intel\Compiler\11.1\060\bin\iclvars.bat" intel64
+if not exist win64x86 mkdir win64x86
 rm -rf win64x86/*  
 mv x64.log x64.log.bak
 cd win64x86 
 echo %date% Begin build process | tee ../x64.log
-e:\Mingw\opt\bin\cmake.exe -DXOPT_OUTPUT_DIR=ON -G "NMake Makefiles" %* ..\..  | tee -a ../x64.log
+cmake.exe -DXOPT_OUTPUT_DIR=ON -G "NMake Makefiles" %* ..\..  | tee -a ../x64.log
 nmake | tee -a ../x64.log
 echo %date% End build process | tee -a ../x64.log
+nmake install | tee -a ../x64.log
 cd .. 
